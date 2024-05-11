7a51734d0f7c64ecd52810a5a3494544be464174
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-05-11 14:03:04 +0530

    create navigation component and refactoring

---
+++ src/components/navigation.jsx
@@ -0,0 +1,20 @@
+
+function Navigation() {
+    return (
+        <div>
+      <nav>
+        <div className='Logo'>
+          <img src="./images/KX.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#" >Home</li>
+          <li href="#">Location</li>
+          <li href="#">Contact</li>
+          <li href="#">About</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+)
+}
+export default Navigation;
